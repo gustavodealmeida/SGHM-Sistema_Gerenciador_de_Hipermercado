05416a626225acb023c9c18cdf99afb1ee7f1130
Daniel de Abreu <abreud@alunos.utfpr.edu.br>
2019-06-04 10:06:44 -0300

    Classe pessoa adicionada

--- src/main/java/com/utfpr/sghm/entidades/Funcionario.java
+++ src/main/java/com/utfpr/sghm/entidades/Funcionario.java
@@ -1,9 +1,69 @@
-
 package com.utfpr.sghm.entidades;
 
-public class Funcionario {
+import jdk.internal.jline.internal.TestAccessible;
+import main.java.com.utfpr.sghm.entidades.Pessoa;
+
+import java.lang.annotation.Inherited;
+import java.security.Identity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "FUNCIONARIO")
+public class Funcionario extends Pessoa{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="car_nome")
     private String cargo;
-    private String tipoFuncionario;
+    @Column(name="func_cnh")
     private String cnh;
-    private Endereco endereco;
+    
+    //metodo construtor com e sem parametros
+    Funcionario(){
+        this.cargo = "";
+        this.tipoFuncao = "";
+        this.cnh = "";
+    }
+
+    Funcionario(String cargo, String tipoFuncao, String cnh){
+        this.cargo = cargo;
+        this.tipoFuncao = tipoFuncao;
+        this.cnh = cnh;    
+    }
+
+    //getters & setters
+    public void setCargo(String cargo){
+        this.cargo = cargo;
+    }
+
+    public void setTipoFuncao(String tipoFuncao){
+        this.tipoFuncao = tipoFuncao;
+    }
+
+    public void setCNH(String cnh){
+        this.cnh = cnh;
+    }
+
+    public String getCargo(){
+        return this.cargo;
+    }
+
+    public String getTipoFuncao(){
+        return this.tipoFuncao;
+    }
+
+    public String getCNH(){
+        return this.cnh;
+    }
+
+    public void venderProduto(){}
+
+    public void cadastrarCliente(){}
+
+
 }
